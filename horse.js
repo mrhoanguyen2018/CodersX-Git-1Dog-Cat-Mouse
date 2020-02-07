556d5a0f6caa0772b0909035bbe1f435a7ac4d30
HoaPha <mrhoanguyen2018@gmail.com>
2020-02-07 10:09:41 +0700

    Add new method run and jump for Horse class

--- horse.js
+++ horse.js
@@ -1,6 +1,13 @@
 function Horse(){
-
     
+}
+
+Horse.prototype.run = function(){
+    Console.log('Running....');
+}
+
+Horse.prototype.jump = function(){
+    Console.log('Jumped....');
 }
 
 module.exports = Horse;
